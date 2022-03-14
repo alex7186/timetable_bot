8ab089f650b4bd4f5dcd9e8bcc09b0414d0e70f0
alex7186 <sasha2649058@mail.ru>
2022-03-14 10:10:19 +0300

    2

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 start:
+	cd ~/shared/scripts/timetable_bot; git pull https://github.com/alex7186/timetable_bot master
 	pip3 install -r requirements.txt
 	python3 crontab_manager.py start
 
--- crontab_manager.py
+++ crontab_manager.py
@@ -6,6 +6,10 @@ args = list(sys.argv)[1:]
 if 'start' in args:
     cron = CronTab(user=True)
 
+    for existing_job in cron:
+        if existing_job.comment == 'timetable_bot':
+            cron.remove(existing_job)
+
     job = cron.new(
         command=f"python3 /home/pi/shared/timetable_bot/sending_timetable.py",
         comment="timetable_bot"
@@ -17,8 +21,8 @@ if 'start' in args:
 elif 'stop' in args:
     cron = CronTab(user=True)
 
-    for job in cron:
-        if job.comment == 'timetable_bot':
-            cron.remove(job)
+    for existing_job in cron:
+        if existing_job.comment == 'timetable_bot':
+            cron.remove(existing_job)
             
     cron.write()
