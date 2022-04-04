cee96defcdc6513a660d9192a9490a66f871b228
alex7186 <sasha2649058@mail.ru>
2022-04-04 11:30:05 +0300

    autocommit 2022-04-04  11:30:05

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ CURRENT_CONFIG = _get_current_config(SCRIPT_PATH)
 DOWNLOAD_LINK = CURRENT_CONFIG["DOWNLOAD_LINK"]
 FONT_PATH = CURRENT_CONFIG["FONT_PATH"]
 
+
 async def fucking_async(telegram_id, target_group):
     table = MakeTable(
         target_group,
